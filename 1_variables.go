ab6332b327759a6ddb10901a28f3a18116bed1c0
Kristijan Stefanoski <kristijan.stefanoski@yahoo.com>
2022-03-19 13:32:18 +0100

    Add bool and byte slice

--- 1_variables.go
+++ 1_variables.go
@@ -1,7 +1,9 @@
-// All variables must be used
-// Lower case scoped to package, uppercase exposed outside
-// camelCase preferred, capitalize acronyms (ex. URL)
-// Use 'strcov' package for strings conversion
+/*
+All variables must be used
+Lower case scoped to package, uppercase exposed outside
+camelCase preferred, capitalize acronyms (ex. URL)
+Use 'strcov' package for strings conversion
+*/
 package main
 
 import (
@@ -17,8 +19,20 @@ func main() {
 	var j = 30.5
 	k := 40
 	theString := "The string"
+	byteSlice := []byte(theString)
 	shadowed := 50
 
-	log.Printf(":\n %v, %T\n %v, %T\n %v, %T\n %v, %T\n %v, %T\n %v, %T\n",
-		i, i, j, j, k, k, theString, theString, strconv.Itoa(i), strconv.Itoa(i), shadowed, shadowed)
+	log.Printf(": %v, %T", i, i)
+	log.Printf(": %v, %T", j, j)
+	log.Printf(": %v, %T", k, k)
+	log.Printf(": %v, %T", theString, theString)
+	log.Printf(": %v, %T", strconv.Itoa(i), strconv.Itoa(i))
+	log.Printf(": %v, %T", shadowed, shadowed)
+	log.Printf(": %v, %T", byteSlice, byteSlice)
+	log.Printf(": %v, %T", string(byteSlice), string(byteSlice))
+
+	var theBoolean bool
+	log.Printf(": %v, %T", theBoolean, theBoolean)
+	theBoolean = true
+	log.Printf(": %v, %T", theBoolean, theBoolean)
 }
